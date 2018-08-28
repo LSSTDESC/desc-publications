ad90cae24e825ba257792fc66e2a3a7053d9a886
chee <chee@lsst-pc94267.slac.stanford.edu>
2018-08-28 14:19:25 -0700

    Change filetype check to simplemagic.

--- src/main/java/org/lsstdesc/pubs/FileUploadServlet.java
+++ src/main/java/org/lsstdesc/pubs/FileUploadServlet.java
@@ -1,6 +1,7 @@
 package org.lsstdesc.pubs;
 
 import com.j256.simplemagic.ContentInfoUtil;
+import com.j256.simplemagic.ContentInfo;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -88,7 +89,7 @@ public class FileUploadServlet extends HttpServlet {
                     int nextVersion = dbUtil.getMaxExistingVersion(paperid) + 1;
                     int projId = dbUtil.getProjectForPaper(paperid);
                     checkFileType(file);
-                    checkIfPDF(file);
+//                    checkIfPDF(file);
                     File saveFile = getPathForFile(paperid, projId, nextVersion);
                     file.write(saveFile);
                     dbUtil.insertPaperVersion(paperid, nextVersion, remarks, file.getName(), saveFile.getPath());
@@ -116,9 +117,14 @@ public class FileUploadServlet extends HttpServlet {
     }
     
     private void checkFileType(FileItem item) throws IOException, ServletException {
-        byte[] b = new byte[4];
+//        byte[] b = new byte[4];
         ContentInfoUtil util = new ContentInfoUtil();
-        System.out.println(util.findMatch(item.getInputStream()));
+        ContentInfo info = util.findMatch(item.getInputStream());
+        if (info == null){
+            throw new ServletException ("Unknown content-type. item = " + item);
+        } else {
+            System.out.println("Content-type is: " + info.getName());
+        }
     }
     
 
